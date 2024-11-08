ad6baa32192cdfae833a6576d4d7111963bdd61b
Claudio Heizer <claudio.profile@gmail.com>
2024-11-08 17:28:14 -0300

    teste

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import streamlit as st
-from modules import processar_dados
 
-st.title("Dashboard de Dados Processados")
+st.write("""# Dashboard de Controle de Produção""")
 
-# Carrega e processa os dados
-df_processado = processar_dados()
+# # Carrega e processa os dados
+# df_processado = processar_dados()
 
-# Verifica se o DataFrame foi processado com sucesso
-if df_processado is not None:
-    st.write("Dados processados com sucesso!")
-    st.dataframe(df_processado.head())
-else:
-    st.write("Erro ao processar os dados.")
+# # Verifica se o DataFrame foi processado com sucesso
+# if df_processado is not None:
+#     st.write("Dados processados com sucesso!")
+#     st.dataframe(df_processado.head())
+# else:
+#     st.write("Erro ao processar os dados.")
 
